4c464d68f0d7bb14b36d16241ebb85e06dc63c08
hyunseok4384 <hyunseok4384@naver.com>
2019-02-03 20:34:05 +0900

    첫 번째 강의

---
+++ 4-1.py
@@ -0,0 +1,42 @@
+import sys
+import io
+import pickle #(객체, 텍스트) 직렬화, 역직렬화
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+#파일 이름과 데이터
+bfilename = 'c:/section4/test.bin'
+tfilename = 'c:/section4/text.txt'
+
+data1 = 77
+data2 = "Hello World!"
+data3 = ["car","apple","house"]
+
+#바이너리 쓰기
+with open(bfilename,"wb") as f:
+    pickle.dump(data1,f) #dumps(문자열 직렬화)
+    pickle.dump(data2,f)
+    pickle.dump(data3,f)
+
+#텍스트 쓰기
+with open(tfilename,"wt") as f:
+    f.write(str(data1))
+    f.write("\n")
+    f.write(data2)
+    f.write("\n")
+    f.writelines('\n'.join(data3))
+
+#바이너리 읽기
+with open(bfilename,"rb") as f:
+    b = pickle.load(f) #loads(문자열 역직렬화)
+    print(type(b), " Binary Read1 | ",b)
+    b=pickle.load(f)
+    print(type(b), " Binary Read2 | ",b)
+    b=pickle.load(f)
+    print(type(b), " Binary Read3 | ",b)
+
+#텍스트 읽기
+with open(tfilename,"rt") as f:
+    for i, line in enumerate(f,1):
+        print(type(line)," Text Read " + str(i) + "|", line,end="")
